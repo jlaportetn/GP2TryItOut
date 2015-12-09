75122ed4bd206ee1cb683a84c0ddb9de4c2e84a3
jlaportetn <jlaporte81@gmail.com>
2015-12-09 15:46:57 -0600

    Added pg 83

---
+++ Pg83/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+            int[] copyOfNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                copyOfNumbers[i] = numbers[i];
+                Console.WriteLine(numbers[i]);
+            }
+            for (int i = 0; i < copyOfNumbers.Length; i++)
+            {
+                Console.WriteLine(copyOfNumbers[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
