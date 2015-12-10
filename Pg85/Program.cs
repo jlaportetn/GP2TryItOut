c7e97fc6e152eeb093b1e2e32b343c10a23728d3
jlaportetn <jlaporte81@gmail.com>
2015-12-10 14:46:05 -0600

    Added Pg 85

---
+++ Pg85/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 99, 43, 55, 88 };
+            int sum = 0;
+            float average = 0;
+
+            foreach (int number in numbers)
+            sum += number;
+            average = (float)sum / numbers.Length;
+            {
+                Console.WriteLine("The sum of the numbers is " + sum);
+                Console.WriteLine("The average of the numbers is " + average);
+            }
+            Console.ReadKey();
+        }
+    }
+}
